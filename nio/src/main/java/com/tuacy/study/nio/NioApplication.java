fd775f1bfe30af1d97900dced73de983a2fe2b6c
tuacy <1007178106@qq.com>
2019-07-22 16:56:44 +0800

    初始化工程

---
+++ nio/src/main/java/com/tuacy/study/nio/NioApplication.java
@@ -0,0 +1,14 @@
+package com.tuacy.study.nio;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NioApplication.class, args);
+    }
+
+}
---
+++ nio/src/test/java/com/tuacy/study/nio/ByteBufferTest.java
@@ -0,0 +1,5 @@
+package com.tuacy.study.nio;
+
+public class ByteBufferTest {
+
+}
