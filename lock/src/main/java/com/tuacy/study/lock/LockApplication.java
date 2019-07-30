e8db7d7ad9dab32fb70c4b1637b73e9e397fa291
wuyx <1007178106@qq.com>
2019-07-30 08:41:01 +0800

    java 锁学习

---
+++ lock/src/main/java/com/tuacy/study/lock/LockApplication.java
@@ -0,0 +1,14 @@
+package com.tuacy.study.lock;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockApplication.class, args);
+    }
+
+}
