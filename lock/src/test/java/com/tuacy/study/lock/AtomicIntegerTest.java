b277c1f638b3909d25dfe890fde1ee2f30ce0ea2
wuyx <1007178106@qq.com>
2019-07-30 15:09:03 +0800

    java 锁学习

--- lock/src/test/java/com/tuacy/study/lock/AtomicIntegerTest.java
+++ lock/src/test/java/com/tuacy/study/lock/AtomicIntegerTest.java
@@ -3,6 +3,7 @@ package com.tuacy.study.lock;
 import org.junit.Test;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
 /**
  * @name: AtomicIntegerTest
@@ -16,10 +17,33 @@ public class AtomicIntegerTest {
     private AtomicInteger atomicInteger = new AtomicInteger();
 
     @Test
-    public void getAndSet() {
+    public void atomicInteger() {
         System.out.println(atomicInteger.getAndSet(10));
 
         System.out.println(atomicInteger.getAndSet(20));
+    }
+
+    public static final AtomicIntegerFieldUpdater<Person> fieldUpdater = AtomicIntegerFieldUpdater.newUpdater(Person.class, "age");
+
+    @Test
+    public void atomicIntegerFieldUpdater() {
+
+        // 可以在多个线程里面操作，线程安全
+        Person person = new Person();
+        fieldUpdater.set(person, 10);
+
+    }
+
+    class Person {
+        private volatile int age;
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
     }
 
 }
