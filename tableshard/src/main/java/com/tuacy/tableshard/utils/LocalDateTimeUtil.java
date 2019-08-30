b8985c0a90760d4ffc09ae5ca9d226b1449020ae
wuyx <1007178106@qq.com>
2019-08-30 17:46:36 +0800

    table shard 分表实现

---
+++ tableshard/src/main/java/com/tuacy/tableshard/utils/LocalDateTimeUtil.java
@@ -0,0 +1,115 @@
+package com.tuacy.tableshard.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.Date;
+
+/**
+ * @name: LocalDateTimeUtil
+ * @author: tuacy.
+ * @date: 2019/7/3.
+ * @version: 1.0
+ * @Description: LocalDateTime帮助类
+ */
+public class LocalDateTimeUtil {
+
+    //获取当前时间的LocalDateTime对象
+    //LocalDateTime.now();
+
+    //根据年月日构建LocalDateTime
+    //LocalDateTime.of();
+
+    //比较日期先后
+    //LocalDateTime.now().isBefore(),
+    //LocalDateTime.now().isAfter(),
+
+    //Date转换为LocalDateTime
+    public static LocalDateTime convertDateToLDT(Date date) {
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+    }
+
+    //LocalDateTime转换为Date
+    public static Date convertLDTToDate(LocalDateTime time) {
+        return Date.from(time.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+
+    //获取指定日期的毫秒
+    public static Long getMilliByTime(LocalDateTime time) {
+        return time.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    }
+
+    //获取指定日期的秒
+    public static Long getSecondsByTime(LocalDateTime time) {
+        return time.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond();
+    }
+
+    //获取指定时间的指定格式
+    public static String formatTime(LocalDateTime time, String pattern) {
+        return time.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    // 获取指定日期的指定格式
+    public static String formatDate(LocalDate date, String pattern) {
+        return date.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    //获取当前时间的指定格式
+    public static String formatNow(String pattern) {
+        return formatTime(LocalDateTime.now(), pattern);
+    }
+
+    //日期加上一个数,根据field不同加不同值,field为ChronoUnit.*
+    public static LocalDateTime plus(LocalDateTime time, long number, TemporalUnit field) {
+        return time.plus(number, field);
+    }
+
+    //日期减去一个数,根据field不同减不同值,field参数为ChronoUnit.*
+    public static LocalDateTime minu(LocalDateTime time, long number, TemporalUnit field) {
+        return time.minus(number, field);
+    }
+
+    /**
+     * 获取两个日期的差  field参数为ChronoUnit.*
+     *
+     * @param startTime 开始时间
+     * @param endTime   结束时间
+     * @param field     单位(年月日时分秒)
+     * @return 时间差
+     */
+    public static long betweenTwoTime(LocalDateTime startTime, LocalDateTime endTime, ChronoUnit field) {
+        Period period = Period.between(LocalDate.from(startTime), LocalDate.from(endTime));
+        if (field == ChronoUnit.YEARS) return period.getYears();
+        if (field == ChronoUnit.MONTHS) return period.getYears() * 12 + period.getMonths();
+        return field.between(startTime, endTime);
+    }
+
+    //获取一天的开始时间，2017,7,22 00:00
+    public static LocalDateTime getDayStart(LocalDateTime time) {
+        return time.withHour(0)
+                .withMinute(0)
+                .withSecond(0)
+                .withNano(0);
+    }
+
+    //获取一天的结束时间，2017,7,22 23:59:59.999999999
+    public static LocalDateTime getDayEnd(LocalDateTime time) {
+        return time.withHour(23)
+                .withMinute(59)
+                .withSecond(59)
+                .withNano(999999999);
+    }
+
+    /**
+     * Returns the number of milliseconds since January 1, 1970, 00:00:00 GMT
+     */
+    public static long getTime(LocalDateTime localDateTime) {
+        return convertLDTToDate(localDateTime).getTime();
+    }
+
+}
---
+++ tableshard/src/main/java/com/tuacy/tableshard/utils/Tests.java
@@ -0,0 +1,53 @@
+package com.tuacy.tableshard.utils;
+
+import java.time.LocalDateTime;
+
+/**
+ * @name: Tests
+ * @author: tuacy.
+ * @date: 2019/8/30.
+ * @version: 1.0
+ * @Description:
+ */
+public class Tests {
+
+    /**
+     * 日期类型转换为包含“年/月/日/时/分/秒/毫秒”的long型数据。
+     * 日期范围：2000-1-1 ~ 2099-1-1，与DateTimeMinValue、DateTimeMaxValue一致。
+     * 示例：2015-02-12 23:03:59.321 转换目标为：20150212230359321
+     */
+    public static long dateTime2Long(LocalDateTime tm) {
+        return tm.getNano() / 1000000 +
+                1000L * tm.getSecond() +
+                100000L * tm.getMinute() +
+                10000000L * tm.getHour() +
+                1000000000L * tm.getDayOfMonth() +
+                100000000000L * tm.getMonthValue() +
+                10000000000000L * tm.getYear();
+    }
+
+    /**
+     * 包含“年/月/日/时/分/秒/毫秒”的BCD码转换为日期类型。
+     * 日期范围：2000-1-1 ~ 2099-1-1，与DateTimeMinValue、DateTimeMaxValue一致。
+     * 示例：20150212230359321 转换目标为：2015-02-12 23:03:59.321
+     */
+    public static LocalDateTime long2DateTime(long val) {
+
+        long year = val / 10000000000000L;
+
+        long month = (val / 100000000000L) % 100L;
+
+        long day = (val / 1000000000L) % 100L;
+
+        long hour = (val / 10000000L) % 100L;
+
+        long minute = (val / 100000L) % 100L;
+
+        long second = (val / 1000L) % 100L;
+
+        long millSecond = val % 1000L;
+
+        return LocalDateTime.of((int) year, (int) month, (int) day, (int) hour, (int) minute, (int) second, (int) millSecond);
+    }
+
+}
