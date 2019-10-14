b15dc66daaaa0a2a33c3f558461dbaa90e78087c
wuyx <1007178106@qq.com>
2019-10-14 20:53:09 +0800

    SpringBoot 日志集成学习

--- logback/src/main/java/com/tuacy/logback/LogBackApplication.java
+++ logback/src/main/java/com/tuacy/logback/LogBackApplication.java
@@ -1,10 +1,14 @@
 package com.tuacy.logback;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
 public class LogBackApplication {
+
+    private Logger logger = LoggerFactory.getLogger(LogBackApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(LogBackApplication.class, args);
