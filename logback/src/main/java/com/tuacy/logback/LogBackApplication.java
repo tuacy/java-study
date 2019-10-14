072cd072a8ba06a0817ed7e15a7e3ce5188fd51d
wuyx <1007178106@qq.com>
2019-10-14 19:37:03 +0800

    SpringBoot 日志集成学习

---
+++ logback/src/main/java/com/tuacy/logback/LogBackApplication.java
@@ -0,0 +1,20 @@
+package com.tuacy.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class LogBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogBackApplication.class, args);
+        // 我们已经添加了spring-boot-starter-web，所以下面一段代码可以去掉了
+        try {
+            // 阻塞住，要不然一跑完就结束了，因为我这里没有添加starter-web
+            Thread.currentThread().join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
