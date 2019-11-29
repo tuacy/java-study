033a709e66bd7c8b2676934a37c460e125e51e72
tuacy <1007178106@qq.com>
2019-11-29 23:36:10 +0800

    Spring Boot学习

---
+++ springboot/src/main/java/com/tuacy/study/springboot/configuration/RoleUserInfo.java
@@ -0,0 +1,33 @@
+package com.tuacy.study.springboot.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource(value = "classpath:role.yml")
+@ConfigurationProperties(prefix = "admin.user")
+public class RoleUserInfo {
+
+    @Value("${name}")
+    private String name;
+    @Value("${age}")
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
