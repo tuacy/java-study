ef73543427c017de1f10c17c3f918b0528ee6373
wuyx <1007178106@qq.com>
2019-12-09 14:10:50 +0800

    security 认证+授权 使用

--- core/security/src/main/java/com/tuacy/security/SecurityApplication.java
+++ core/security/src/main/java/com/tuacy/security/SecurityApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @name: SecurityApplication
@@ -15,6 +17,8 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableCaching
 @MapperScan(basePackages = "com.tuacy.security.mapper")
+@EnableDiscoveryClient
+@EnableFeignClients
 public class SecurityApplication {
 
     public static void main(String[] args) {
