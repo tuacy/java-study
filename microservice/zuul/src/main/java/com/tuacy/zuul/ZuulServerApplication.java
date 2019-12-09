b005ea6718da6091b51912ff1bfdbbee84b74d60
wuyx <1007178106@qq.com>
2019-12-09 12:22:39 +0800

    security 认证+授权 使用

---
+++ microservice/zuul/src/main/java/com/tuacy/zuul/ZuulServerApplication.java
@@ -0,0 +1,18 @@
+package com.tuacy.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
+public class ZuulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
+}
