0438efea7a00de8f3d1583348df08e6536f8af59
wuyx <1007178106@qq.com>
2019-12-19 15:09:10 +0800

    commons-jexl 的使用。实现简单字符串转换成代码的功能

---
+++ jexl/src/main/java/com/tuacy/jexl/JexlApplication.java
@@ -0,0 +1,13 @@
+package com.tuacy.jexl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JexlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JexlApplication.class, args);
+    }
+
+}
---
+++ jexl/src/test/java/com/tuacy/jexl/JexlTest.java
@@ -0,0 +1,60 @@
+package com.tuacy.jexl;
+
+import org.apache.commons.jexl2.Expression;
+import org.apache.commons.jexl2.JexlContext;
+import org.apache.commons.jexl2.JexlEngine;
+import org.apache.commons.jexl2.MapContext;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @name: JexlTest
+ * @author: tuacy.
+ * @date: 2019/12/19.
+ * @version: 1.0
+ * @Description:
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class JexlTest {
+
+    @Test
+    public void test() {
+        try {
+            Map<String, Object> map = new HashMap<>();
+            map.put("money", 2100);
+            String expression = "money>=2000&&money<=4000";
+            Object code = convertToCode(expression, map);
+            if ((boolean)code) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 替换逻辑表达式中的模板
+     */
+    public static Object convertToCode(String jexlExp, Map<String, Object> map) {
+        JexlEngine jexlEngine = new JexlEngine();
+        Expression expression = jexlEngine.createExpression(jexlExp);
+        JexlContext jexlContext = new MapContext();
+        for (String key : map.keySet()) {
+            jexlContext.set(key, map.get(key));
+        }
+        if (null == expression.evaluate(jexlContext)) {
+            return "";
+        }
+        return expression.evaluate(jexlContext);
+    }
+
+}
